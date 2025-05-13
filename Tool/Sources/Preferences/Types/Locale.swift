4bf7f5d233b654e50dc7e3764afc771ce31a6d6b
ROBOTVIPONE <191507985+hoangtu1sos1hot@users.noreply.github.com>
2025-05-13 20:02:40 +0700

    Update Locale.swift
    Co-Authored-By: Andrew Zures <andrew.zures@gmail.com>

--- Tool/Sources/Preferences/Types/Locale.swift
+++ Tool/Sources/Preferences/Types/Locale.swift
@@ -3,7 +3,6 @@ import Foundation
 public extension Locale {
     static var availableLocalizedLocales: [String] {
         let localizedLocales = Locale.isoLanguageCodes.compactMap {
-            Locale(identifier: "en-US").localizedString(forLanguageCode: $0)
         }
         .sorted()
         return localizedLocales
